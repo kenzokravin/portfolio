228dc0b038992c884df4a3b48b8e1f20efa709ba
kenzokravin <kenzokravin@gmail.com>
2025-03-24 12:51:24 +1100

    Added Custom font and text

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # portfolio
 portfolio design
+
+To check server, right-click index.html and click open on live server.
--- sketch.js
+++ sketch.js
@@ -1,7 +1,33 @@
+let windowHeight = window.innerHeight;
+let windowWidth = window.innerWidth;
+let outfitFont;
+
+function preload() {
+ outfitFont = loadFont('assets/Outfit-Medium.ttf');
+}
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth = window.innerWidth, windowHeight = window.innerHeight);
+
+  textFont(outfitFont);
+  textAlign(CENTER, CENTER); // Align text to center both horizontally and vertically
+ 
 }
 
 function draw() {
   background(220);
+
+  let textSizeValue = min(windowWidth, windowHeight) * 0.08; 
+  textSize(textSizeValue);
+  //circle in the center with a width of 100
+circle(200,200,100);
+text("Hello, World!", windowWidth / 2, windowHeight / 2);
 }
+
+function windowResized() {
+  windowWidth = window.innerWidth;
+  windowHeight = window.innerHeight;
+  resizeCanvas(windowWidth, windowHeight);
+}
+
