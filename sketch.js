d2cf10451ffe660e0e252f00c81ee7e465626b1b
kenzokravin <kenzokravin@gmail.com>
2025-03-24 13:07:41 +1100

    Added random number function

--- sketch.js
+++ sketch.js
@@ -2,6 +2,13 @@ let windowHeight = window.innerHeight;
 let windowWidth = window.innerWidth;
 let outfitFont;
 
+let targetText = "<hello, i'm angus :)>";
+let currentText = [];
+let iterationNumberForLetter = [];
+let iterationNum = 0;
+let characters = "ABCEFHIJKLMNOPQSUXYZabcefghijlmnoprstuwxyz0124679!?.,[}]}/$%&*@':)<> ";
+let swapSpeed = 50; // Speed of letter swaps in milliseconds
+
 function preload() {
  outfitFont = loadFont('assets/Outfit-Medium.ttf');
 }
@@ -13,6 +20,13 @@ function setup() {
   textFont(outfitFont);
   textAlign(CENTER, CENTER); // Align text to center both horizontally and vertically
  
+  //Initializing random letters.
+  for (let i = 0; i < targetText.length; i++) {
+    currentText[i] = randomCharacter();
+    iterationNumberForLetter[i] = randomNumber();
+  }
+
+  setInterval(swapLetters, swapSpeed);
 }
 
 function draw() {
@@ -21,13 +35,40 @@ function draw() {
   let textSizeValue = min(windowWidth, windowHeight) * 0.08; 
   textSize(textSizeValue);
   //circle in the center with a width of 100
-circle(200,200,100);
-text("Hello, World!", windowWidth / 2, windowHeight / 2);
+
+text(currentText.join(""), windowWidth / 2, windowHeight / 2);
 }
 
 function windowResized() {
   windowWidth = window.innerWidth;
   windowHeight = window.innerHeight;
   resizeCanvas(windowWidth, windowHeight);
+}
+
+function swapLetters() {
+  for (let i = 0; i < targetText.length; i++) {
+
+    if(iterationNumberForLetter[i] <= iterationNum) {
+      currentText[i] = targetText[i];
+    }
+
+
+    if (currentText[i] !== targetText[i]) {
+
+      iterationNum++;
+
+
+      currentText[i] = randomCharacter(); // Keep swapping until it matches
+    }
+  }
+}
+
+// Function to get a random character
+function randomCharacter() {
+  return characters.charAt(floor(random(characters.length)));
+}
+
+function randomNumber() {
+  return Math.floor(Math.random() * 150 + 1);
 }
 
